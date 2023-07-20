32b8a180d7e482c485d46bcf514ce4752d49545b
defense <1900013009@pku.edu.cn>
2023-07-20 11:05:08 +0800

    add_pos_ridge_model

--- ridgeSegModule/ridge_segmentation.py
+++ ridgeSegModule/ridge_segmentation.py
@@ -7,13 +7,15 @@ def generate_ridge(data_path):
     os.makedirs(os.path.join(data_path,'ridge_points'),exist_ok=True)
     os.system(f"rm -rf {os.path.join(data_path,'ridge_mask')}/*")
     os.system(f"rm -rf {os.path.join(data_path,'ridge_points')}/*")
-    processer=ridge_segmentation_processer('point',5,100)
+    processer=ridge_segmentation_processer('point',5,150)
     for split in splits:
         with open(os.path.join(data_path,'annotations',f'{split}.json'),'r') as f:
             data_list=json.load(f)
         annotation_res=[]
         for data in data_list:
+            pos_path=os.path.join(data_path,'pos_embed',data['image_name'].split('.')[0]+'.pt')
             _,annotes=processer(data['image_path'],
+                                position_path=pos_path,
                                 class_=data['class'],
                                 save_path=os.path.join(
                 data_path,'ridge_mask',data['image_name']))
--- ridgeSegModule/ridge_segmentation_prcesser.py
+++ ridgeSegModule/ridge_segmentation_prcesser.py
@@ -53,10 +53,10 @@ class ridge_segmentation_processer():
     def __init__(self,mode,point_number,point_dis=50):
         self.mode=mode
         assert mode in ['mask','point']
+
         if mode=='point':
             self.point_number=point_number
             self.point_dis=point_dis
-
         self.model=FR_UNet().cuda()
         self.model.load_state_dict(
                 torch.load('./ridgeSegModule/checkpoint/ridge_seg.pth'))
@@ -66,27 +66,35 @@ class ridge_segmentation_processer():
                 mean=[0.4623, 0.3856, 0.2822],
                 std=[0.2527, 0.1889, 0.1334])])
 
-    def __call__(self,img_path,class_,save_path=None):
+    def __call__(self,img_path,position_path,class_,save_path=None):
         with torch.no_grad():
             img=Image.open(img_path)
-            decomposed_images = decompose_image_into_tensors(self.img_transforms(img))
-            composed_mask = []
-            for decomposed_image in decomposed_images:
-                decomposed_image = decomposed_image.unsqueeze(0).cuda() 
-                mask_part = self.model(decomposed_image).detach().cpu()
-                mask_part = torch.sigmoid(mask_part)
-                composed_mask.append(mask_part)
+            # decomposed_images = decompose_image_into_tensors(self.img_transforms(img))
+            # composed_mask = []
+            # for decomposed_image in decomposed_images:
+            #     decomposed_image = decomposed_image.unsqueeze(0).cuda() 
+            #     mask_part = self.model(decomposed_image).detach().cpu()
+            #     mask_part = torch.sigmoid(mask_part)
+            #     composed_mask.append(mask_part)
 
-            mask = compose_tensors_into_image(composed_mask)
-            
+            # mask = compose_tensors_into_image(composed_mask)
+            img_tensor = self.img_transforms(img)
+            pos_embed=torch.load(position_path)
+            pos_embed=F.interpolate(pos_embed[None,None,:,:], size=img_tensor.shape[-2:], mode='nearest')
+            pos_embed=pos_embed.squeeze()
+
+            img=img_tensor.unsqueeze(0).cuda()
+            pos_embed=pos_embed.unsqueeze(0).cuda()
+            output_img = self.model((img,pos_embed)).cpu()
+            # Resize the output to the original image size
+            mask=torch.sigmoid(output_img).numpy()
             if save_path:
                 cv2.imwrite(save_path,
-                    np.uint8(mask.numpy()*255))
+                    np.uint8(mask*255))
             assert save_path is not None
             if self.mode=='point':
-                mask_numpy = mask.numpy()
                 maxvals_list, preds_list=k_max_values_and_indices(
-                    mask_numpy,self.point_number,self.point_dis)
+                    mask,self.point_number,self.point_dis)
                 maxvals_list=maxvals_list.tolist()
                 preds_list=preds_list.tolist()
                 data={
