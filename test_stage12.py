0bb6f0fc33f18912eb46c0d60f10687b461d03dd
defense <1900013009@pku.edu.cn>
2023-09-16 19:00:09 +0800

    add

--- test_stage12.py
+++ test_stage12.py
@@ -3,81 +3,63 @@ import os
 import torch
 from torchvision import transforms
 from config import get_config
-from utils_ import get_instance
-import models.heatmap as models
+from utils_ import to_device
+from models import build_model
 import numpy as np
 from PIL import Image
-from utils_ import acc,auc,auc_sens
-import shutil
+from utils_ import acc, auc, auc_sens
+
 # Parse arguments
 args = get_config()
 
 # Init the result file to store the pytorch model and other mid-result
 result_path = args.result_path
-os.makedirs(result_path,exist_ok=True)
+os.makedirs(result_path, exist_ok=True)
 print(f"the mid-result and the pytorch model will be stored in {result_path}")
 
 # Create the model and criterion
-model = get_instance(models, args.configs.MODEL.NAME,args.configs,
-                         num_classes=args.configs.NUM_CLASS)
-
+model,_ = build_model(configs=args.configs['model'])
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 model.load_state_dict(
-    torch.load(args.save_name))
-print(f"load the checkpoint in {args.save_name}")
+    torch.load(os.path.join(args.save_dir, f"{args.split_name}_{args.save_name}")))
+print("load the checkpoint in {}".format(os.path.join(
+    args.save_dir, f"{args.split_name}_{args.save_name}")))
 model.eval()
 
 # Create the dataset and data loader
-data_path=os.path.join(args.path_tar)
 
 # Create the visualizations directory if it doesn't exist
-with open(os.path.join(data_path,'ridge_points','test.json'),'r') as f:
-    ridge_seg_list=json.load(f)
+with open(os.path.join(args.data_path, 'split', f'{args.split_name}.json'), 'r') as f:
+    split_list = json.load(f)['test']
+with open(os.path.join(args.data_path, 'annotations.json'), 'r') as f:
+    data_dict = json.load(f)
+
 all_targets = []
 all_outputs = []
-all_scores=[]
-heatmap_transform=transforms.Compose([
-    transforms.Resize((300,300)),
-    transforms.ToTensor()])
-print(f"test name number {len(ridge_seg_list)}")
-cnt_max=5
-f=open('./wrong_list.txt','w')
-os.makedirs(os.path.join(result_path,'wrong_list'),exist_ok=True)
-os.system(f"rm -rf {os.path.join(result_path,'wrong_list')}/*")
-os.makedirs(os.path.join(result_path,'wrong_list','p0t1','image'),exist_ok=True)
-os.makedirs(os.path.join(result_path,'wrong_list','p0t1','mask'),exist_ok=True)
-os.makedirs(os.path.join(result_path,'wrong_list','p1t0','image'),exist_ok=True)
-os.makedirs(os.path.join(result_path,'wrong_list','p1t0','mask'),exist_ok=True)
-
+all_scores = []
+print(f"test name number {len(split_list)}")
+heatmap_process = transforms.Compose([
+    transforms.Resize(args.configs['image_resize']),
+    transforms.ToTensor()
+])
 with torch.no_grad():
-    for data in ridge_seg_list:
-        image_path=data['image_path']
-        label=data['class']
+    for image_name in split_list:
+        data = data_dict[image_name]
+        heatmap = Image.open(data['ridge_seg']['heatmap_path']).convert('RGB')
+        label = data['stage']
         if label > 0:
             label = 1
-        image_name= data['image_name']
-        heatmap=Image.open(data['heatmap_path'])
-        heatmap=heatmap_transform(heatmap).repeat(3,1,1).to(device)
+        heatmap = heatmap_process(heatmap)
+        heatmap= to_device(heatmap,device)
         outputs = model(heatmap.unsqueeze(0))
         probs = torch.softmax(outputs, dim=1)
         predicted_labels = torch.argmax(outputs, dim=1).squeeze().cpu()
-        if label != predicted_labels:
-            f.write(f"{image_name} pred: {predicted_labels} label: {label} path: {data['heatmap_path']}\n")
-            if label==1:
-                shutil.copy(data['heatmap_path'],os.path.join(result_path,'wrong_list','p0t1','mask',data['image_name']))
-                shutil.copy(data['image_path'],os.path.join(result_path,'wrong_list','p0t1','image',data['image_name']))
-            else:
-                shutil.copy(data['heatmap_path'],os.path.join(result_path,'wrong_list','p1t0','mask',data['image_name']))
-                shutil.copy(data['image_path'],os.path.join(result_path,'wrong_list','p1t0','image',data['image_name']))
         all_targets.append(label)
         all_outputs.append(int(predicted_labels))
         all_scores.append(probs.detach().cpu())
-all_targets=np.array(all_targets)
-all_outputs=np.array(all_outputs)
-all_scores=torch.cat(all_scores,dim=0).numpy()
+all_targets = np.array(all_targets)
+all_outputs = np.array(all_outputs)
+all_scores = torch.cat(all_scores, dim=0).numpy()
+print(f"acc: {acc(all_targets,all_outputs)} | auc: {auc_sens(all_targets,all_outputs)}")
 print("Finished testing!")
-# print(f"acc: {acc(all_targets,all_outputs)} | auc: {auc(all_targets,all_scores)}")
-# all_targets[all_targets>0]=1
-# all_outputs[all_outputs>0]=1
-print(f"sens acc: {acc(all_targets,all_outputs)}| {auc_sens(all_targets,all_outputs)}" )
--- test_stage23.py
+++ test_stage23.py
@@ -1,93 +1,65 @@
 import json
 import os
 import torch
-from cleansing import crop_square
 from torchvision import transforms
 from config import get_config
-from utils_ import get_instance,ContrastEnhancement
-import models.crop as models
+from utils_ import to_device
+from models import build_model
 import numpy as np
 from PIL import Image
-from utils_ import acc,auc,auc_sens
+from utils_ import acc, auc, auc_sens
 
 # Parse arguments
 args = get_config()
 
 # Init the result file to store the pytorch model and other mid-result
 result_path = args.result_path
-os.makedirs(result_path,exist_ok=True)
+os.makedirs(result_path, exist_ok=True)
 print(f"the mid-result and the pytorch model will be stored in {result_path}")
 
 # Create the model and criterion
-model = get_instance(models, args.configs.MODEL.NAME,args.configs,
-                         num_classes=args.configs.NUM_CLASS)
-
+model,_ = build_model(configs=args.configs['model'])
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 model.load_state_dict(
-    torch.load(args.save_name))
-print(f"load the checkpoint in {args.save_name}")
+    torch.load(os.path.join(args.save_dir, f"{args.split_name}_{args.save_name}")))
+print("load the checkpoint in {}".format(os.path.join(
+    args.save_dir, f"{args.split_name}_{args.save_name}")))
 model.eval()
 
 # Create the dataset and data loader
-data_path=os.path.join(args.path_tar)
 
 # Create the visualizations directory if it doesn't exist
-with open(os.path.join(data_path,'ridge_points','test.json'),'r') as f:
-    ridge_seg_list=json.load(f)
+with open(os.path.join(args.data_path, 'split', f'{args.split_name}.json'), 'r') as f:
+    split_list = json.load(f)['test']
+with open(os.path.join(args.data_path, 'annotations.json'), 'r') as f:
+    data_dict = json.load(f)
+
 all_targets = []
 all_outputs = []
-all_scores=[]
-img_transform=transforms.Compose([
-    ContrastEnhancement(),
-    transforms.Resize((300,300)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.4485, 0.5278, 0.5477], 
-                         std=[0.0910, 0.1079, 0.1301])])
-vessel_transform=transforms.Compose([
-    transforms.Resize((300,300)),
-    transforms.ToTensor()])
-print(f"test name number {len(ridge_seg_list)}")
-cnt_max=5
+all_scores = []
+print(f"test name number {len(split_list)}")
+heatmap_process = transforms.Compose([
+    transforms.Resize(args.configs['image_resize']),
+    transforms.ToTensor()
+])
 with torch.no_grad():
-    for data in ridge_seg_list:
-        image_path=data['image_path']
-        label=data['class']
-        image_name= data['image_name']
-        vessel_path= os.path.join(args.path_tar,'vessel_seg',data['image_name'])
-        ridge_seg_res=data['ridge']
-        cnt=0
-        predict_labels_image=0
-        select_ouput=None
-        for item in ridge_seg_res:
-            y,x=item['coordinate']
-            crop_image=crop_square(image_path,x=x,y=y,
-                        width=300,
-                        )
-            crop_vessel=crop_square(vessel_path,x=x,y=y,
-                        width=300,
-                        )
-            
-            img=img_transform(crop_image).to(device)
-            vessel=vessel_transform(crop_vessel).repeat(3,1,1).to(device)
-            outputs = model([img.unsqueeze(0),vessel.unsqueeze(0)])
-            probs = torch.softmax(outputs, dim=1)
-            predicted_labels = torch.argmax(outputs, dim=1).squeeze().cpu()
-            predict_labels_image=max(predict_labels_image,int(predicted_labels))
-            if select_ouput is None or predicted_labels>predict_labels_image:
-                select_ouput=probs
-                predict_labels_image=predicted_labels
-            cnt+=1
-            if cnt>=cnt_max:
-                break
+    for image_name in split_list:
+        data = data_dict[image_name]
+        heatmap = Image.open(data['ridge_seg']['heatmap_path']).convert('RGB')
+        label = data['stage']
+        if label > 0:
+            label = 1
+        heatmap = heatmap_process(heatmap)
+        heatmap= to_device(heatmap,device)
+        outputs = model(heatmap.unsqueeze(0))
+        probs = torch.softmax(outputs, dim=1)
+        predicted_labels = torch.argmax(outputs, dim=1).squeeze().cpu()
         all_targets.append(label)
-        all_scores.append(select_ouput.detach().cpu())
-        all_outputs.append(predict_labels_image)
-all_targets=np.array(all_targets)
-all_outputs=np.array(all_outputs)
-all_scores=torch.cat(all_scores,dim=0).numpy()
+        all_outputs.append(int(predicted_labels))
+        all_scores.append(probs.detach().cpu())
+all_targets = np.array(all_targets)
+all_outputs = np.array(all_outputs)
+all_scores = torch.cat(all_scores, dim=0).numpy()
+print(f"acc: {acc(all_targets,all_outputs)} | auc: {auc_sens(all_targets,all_outputs)}")
 print("Finished testing!")
-print(f"acc: {acc(all_targets,all_outputs)} | auc: {auc(all_targets,all_scores)}")
-all_targets[all_targets>0]=1
-all_outputs[all_outputs>0]=1
-print(f"sens acc: {acc(all_targets,all_outputs)}| {auc_sens(all_targets,all_outputs)}" )
